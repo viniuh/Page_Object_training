7201943c2500aa461410db604881301d9c402426
Vasya <viniuh96@gmail.comgit config --global user.name Vasyagit config --global user.email viniuh96@gmail.com>
2021-09-21 21:39:18 +0300

    Update the test case

--- pages/main_page.py
+++ pages/main_page.py
@@ -2,12 +2,13 @@ from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
 from .locators import LoginPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
